0424912fd228db8a29fe84db360e4fbc78a66e67
rjwysong92 <rjbrewer92@gmail.com>
2024-11-20 15:21:26 -0500

    React.11.20.24.a

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -18,6 +18,7 @@ urlpatterns = [
     path(route='dealer/<int:dealer_id>', view=views.get_dealer_details, name='dealer_details'),
     path(route='reviews/dealer/<int:dealer_id>', view=views.get_dealer_reviews, name='dealer_reviews'),
     path(route='add_review', view=views.add_review, name='add_review'),
+    path(route='get_dealers/', view=views.get_dealerships, name='get_dealers'),
 
 
     # path for dealer reviews view
