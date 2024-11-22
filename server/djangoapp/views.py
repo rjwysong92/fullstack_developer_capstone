edb3d2732643bedc9f4c12b395068b0d83f7d233
rjwysong92 <rjbrewer92@gmail.com>
2024-11-22 10:05:07 -0500

    dyn.ap.11.22.24.E

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -94,7 +94,7 @@ def get_dealerships(request, state="All"):
     if(state == "All"):
         endpoint = "/fetchDealers"
     else:
-        endpoint = "/fetchDealers/"+state
+        endpoint = "/fetchDealers/"+str(state)
     dealerships = get_request(endpoint)
     return JsonResponse({"status":200,"dealers":dealerships})
 # ...
@@ -102,7 +102,7 @@ def get_dealerships(request, state="All"):
 # Create a `get_dealer_reviews` view to render the reviews of a dealer
 def get_dealer_reviews(request,dealer_id):
     if(dealer_id):
-        endpoint ="/fetchreviews/dealer/"+str(dealer_id)
+        endpoint ="/fetchReviews/dealer/"+str(dealer_id)
         reviews = get_request(endpoint)
         for review_details in reviews:
             response = analyze_review_sentiments(review_details['review'])
@@ -115,7 +115,7 @@ def get_dealer_reviews(request,dealer_id):
 # Create a `get_dealer_details` view to render the dealer details
 def get_dealer_details(request, dealer_id):
     if (dealer_id):
-        endpoint ="/dealer/"+str(dealer_id)
+        endpoint ="/fetchDealer/"+str(dealer_id)
         dealership = get_request(endpoint)
         return JsonResponse({"status":200, "dealer":dealership})
     else:
