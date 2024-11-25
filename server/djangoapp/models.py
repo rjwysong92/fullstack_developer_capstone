d10e2022a7247ae98070283aa7a1c9719ace23de
rjwysong92 <140678370+rjwysong92@users.noreply.github.com>
2024-11-25 15:46:55 -0500

    Update models.11.25.24.py

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,7 +1,7 @@
 # Uncomment the following imports before adding the Model code
 
 from django.db import models
-from django.utils.timezone import now
+# from django.utils.timezone import now
 from django.core.validators import MaxValueValidator, MinValueValidator
 
 
@@ -13,63 +13,14 @@ class CarMake(models.Model):
     description = models.TextField()
     country_of_origin = models.TextField()
     number_of_models = models.IntegerField()
+
     def __str__(self):
-        return self.name 
+        return self.name
 # - Name
 # - Description
 # - Any other fields you would like to include in car make model
 # - __str__ method to print a car make object
 
-
-# <HINT> Create a Car Model model `class CarModel(models.Model):`:
-class CarModel(models.Model):
-    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
-    name = models.CharField(max_length=100)
-    dealer_id = models.IntegerField()
-    CAR_TYPES_CHOICES =  [
-        ('SEDAN', 'Sedan'),
-        ('SUV', 'SUV'),
-        ('WAGON', 'Wagon'),
-        ('TRUCK', 'Truck'),
-        ('COUPE', 'Coupe'),
-    ]
-    type = models.CharField(max_length=15, choices=CAR_TYPES_CHOICES, default='SUV')
-    year = models.IntegerField(default=2024,
-        validators=[
-            MaxValueValidator(2025),
-            MinValueValidator(2015)
-        ])
-    FUEL_TYPE_CHOICES = [
-        ('GASOLINE', 'Gasoline'),
-        ('HYBRID', 'Hybrid'),
-        ('ELECTRIC', 'Electric')
-    ]
-    efficiency_type = models.CharField(
-        max_length=20, 
-        choices=FUEL_TYPE_CHOICES,
-        default='GASOLINE'
-    )
-    mpg = models.IntegerField( 
-        blank=True,
-        null=False,
-        validators=[
-            MaxValueValidator(60),
-            MinValueValidator(0)
-        ])
-    mpc = models.IntegerField(
-        blank=True,
-        null=False,
-        validators=[
-            MaxValueValidator(500),
-            MinValueValidator(0)
-        ])
-    passenger_capacity = models.IntegerField(default=5, 
-        validators=[
-            MaxValueValidator(15),
-            MinValueValidator(2)
-        ])
-    def __str__(self):
-        return self.name
 # - Many-To-One relationship to Car Make model (One Car Make has many
 # Car Models, using ForeignKey field)
 # - Name
@@ -78,3 +29,53 @@ class CarModel(models.Model):
 # - Year (IntegerField) with min value 2015 and max value 2023
 # - Any other fields you would like to include in car model
 # - __str__ method to print a car make object
+# <HINT> Create a Car Model model `class CarModel(models.Model):`:
+class CarModel(models.Model):
+    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    dealer_id = models.IntegerField()
+    CAR_TYPES_CHOICES = [
+        ('SEDAN', 'Sedan'),
+        ('SUV', 'SUV'),
+        ('WAGON', 'Wagon'),
+        ('TRUCK', 'Truck'),
+        ('COUPE', 'Coupe'),
+    ]
+    type = models.CharField(max_length=15, 
+                            choices=CAR_TYPES_CHOICES, 
+                            default='SUV')
+    year = models.IntegerField(default=2024,
+        validators= [
+            MaxValueValidator(2025),
+            MinValueValidator(2015)
+            ])
+    FUEL_TYPE_CHOICES = [
+        ('GASOLINE', 'Gasoline'),
+        ('HYBRID', 'Hybrid'),
+        ('ELECTRIC', 'Electric')]
+    efficiency_type = models.CharField(
+        max_length=20,
+        choices=FUEL_TYPE_CHOICES,
+        default='GASOLINE')
+    mpg = models.IntegerField(
+        blank=True,
+        null=False,
+        validators=[
+            MaxValueValidator(60),
+            MinValueValidator(0)
+            ])
+    mpc = models.IntegerField(
+        blank=True,
+        null=False,
+        validators= [
+            MaxValueValidator(500),
+            MinValueValidator(0)
+            ])
+    passenger_capacity = models.IntegerField(default=5,
+        validators= [
+            MaxValueValidator(15),
+            MinValueValidator(2)
+            ])
+    
+    def __str__(self):
+        return self.name
